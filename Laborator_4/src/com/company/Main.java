e3187ea844fdf85658e41858da750ae6b8a058d1
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-03-16 17:09:44 +0200

    Add files via upload

---
+++ Laborator_4/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import primulPachet.*;
+import primulPachet.subpachet.C;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    A x = new A();
+	    B y = new B();
+	    C z = new C();
+	    x.tip();
+	    y.tip();
+	    z.tip();
+    }
+}
---
+++ Laborator_4/src/primulPachet/subpachet/C.java
@@ -0,0 +1,7 @@
+package primulPachet.subpachet;
+
+public class C {
+    public void tip(){
+        System.out.println("Clasa C");
+    }
+}
