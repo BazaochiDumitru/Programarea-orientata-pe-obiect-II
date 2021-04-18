fd581a53d78fe6a81a99d24ad8210e222d3e71d3
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-04-18 13:17:31 +0300

    Add files via upload

---
+++ Laborator_3/FirstApp/src/firstapp/FirstApp.java
@@ -0,0 +1,24 @@
+package firstapp;
+
+import primulPachet.A;
+import primulPachet.B;
+import primulPachet.subpachet.C;
+
+public class FirstApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        A x = new A();
+        B y = new B();
+        C z = new C();
+        x.tip();
+        y.tip();
+        z.tip();
+        
+        
+    }
+    
+}
