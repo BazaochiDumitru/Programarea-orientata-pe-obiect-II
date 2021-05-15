6704832d1e19a8bf851f365899dc73104ca0a3c1
BazaochiDumitru <54941010+BazaochiDumitru@users.noreply.github.com>
2021-05-15 12:29:52 +0300

    Add files via upload

---
+++ MVC_1/MVC_1/MVC_1/src/pachet1/MVCPatternDemo.java
@@ -0,0 +1,27 @@
+package pachet1;
+
+public class MVCPatternDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//fetch student record based on his roll no from the database
+	      Student model  = retriveStudentFromDatabase();
+
+	      //Create a view : to write student details on console
+	      StudentView view = new StudentView();
+
+	      StudentController controller = new StudentController(model, view);
+
+	      controller.updateView();
+
+	      //update model data
+	      controller.setStudentName("Veronica");
+
+	      controller.updateView();
+	   }
+	   private static Student retriveStudentFromDatabase(){
+	      Student student = new Student();
+	      student.setName("Robert");
+	      student.setRollNo("12");
+	      return student;
+	}
+}
